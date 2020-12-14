e191fdfef787c59e30e01417149fa12085aec161
rezarinaldi <rez4rinaldi@gmail.com>
2020-12-14 17:20:51 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 💭 Chat Web Socket
+
+## Run
+
+```swift
+$ git clone https://github.com/rezarinaldi/chat-web-socket.git
+$ cd chat-web-socket
+$ npm install
+$ node index.js
+$ open web browser http://localhost:3000/
+```
+
+## Reference
+
+- https://socket.io/get-started/chat/
+- https://www.dreamincode.net/forums/topic/410860-socketio-and-nodejs-io-is-not-defined/
---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(3000, () => {
+  console.log("listening on port: 3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
